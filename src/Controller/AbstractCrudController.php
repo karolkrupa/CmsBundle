9bc533fb7f45ba63a7e788514bafd77f59614b1c
Karol Krupa <karo2krupa@gmail.com>
2024-03-11 14:08:39 +0100

    fix

--- src/Controller/AbstractCrudController.php
+++ src/Controller/AbstractCrudController.php
@@ -14,6 +14,7 @@ class AbstractCrudController extends AbstractController
             ...parent::getSubscribedServices(),
             ...[
                 NotificationService::class,
+                PageFactory::class
             ]
         ];
     }
